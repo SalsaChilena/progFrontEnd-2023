e1b25622d0b39c878dac417de2a660cb2f0673a7
Tecnologías de la Información <tic.calama@inacap.cl>
2023-07-07 14:59:30 -0400

    primer uwu

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from "react";
+import ReactDom from "react-dom/client"
+import { Routes, Route, Link } from "react-router-dom"
+
+const App = () => {
+  return (
+    <div>
+      <h1> Hey chiquitita, ¿Como tu ta? </h1>
+      <p> bn uwu </p>
+    </div>
+  )
+}
+
+export default App;
